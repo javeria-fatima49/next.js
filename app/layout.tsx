ea2850e3713d7e37047872b8322fb9a15cb58b7c
javeria <javeriafatima246@gmail.com>
2024-10-18 10:44:16 +0500

    This is first assignment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 "use client";
-// import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./component/Header";
-import Footer from "./component/Footer";
-
-
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
